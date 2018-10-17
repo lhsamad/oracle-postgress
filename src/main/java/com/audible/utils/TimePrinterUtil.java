49b67f5918a516ae732d58555f8f78333a3fa829
luke samad <luksamad@amazon.com>
2018-10-17 09:25:01 -0400

    upgrading, putting back removed file

--- src/main/java/com/audible/utils/TimePrinterUtil.java
+++ src/main/java/com/audible/utils/TimePrinterUtil.java
@@ -1,5 +1,6 @@
 package com.audible.utils;
 
+import com.sun.prism.shader.Solid_TextureYV12_AlphaTest_Loader;
 import java.text.SimpleDateFormat;
 import java.time.Clock;
 import java.time.Instant;
@@ -17,13 +18,33 @@ import org.apache.commons.lang3.time.DateUtils;
 public class TimePrinterUtil {
 
 
-  public static void main(String[] args) throws Exception{
-    LocalDateTime localTime = LocalDateTime.now(ZoneId.of("GMT+02:30"));
-    System.out.println(localTime);
+  static String start = "1539561600";
+  static String end = "1539647999";
 
 
+  public static void main(String arg[]) throws Throwable {
+    String SQL_ONE = "Select * FROM AA_MRPI_ACTIVE_CREDIT_PST WHERE DATE_UPDATED between "+getOracleDate(start)+" and "+getOracleDate(end)+" order by MRPI_ACTIVE_SEQ_NO";
+    System.out.println(SQL_ONE);
 
 
+    String SQL_TWO = "Select * FROM booker.AA_MRPI_ACTIVE_CREDIT_PST WHERE DATE_UPDATED between "+getPostgresDate(start)+" and "+getPostgresDate(end)+" order by MRPI_ACTIVE_SEQ_NO";
+    System.out.println(SQL_TWO);
+
+  }
+
+
+  public static String getPostgresDate(String epoch) throws Throwable{
+    long date = Long.parseLong(epoch);
+    Instant i = Instant.ofEpochSecond(date);
+    System.out.println(i);
+    return "'"+i+"'::timestamp";
+  }
+
+  public static String getOracleDate(String epoch) throws Throwable{
+    long date = Long.parseLong(epoch);
+    Instant i = Instant.ofEpochSecond(date);
+    System.out.println(i);
+    return "to_timestamp('"+i+"', 'yyyy-mm-dd\"T\"hh24:mi:ss.ff3\"Z\"')";
   }
 
 }
